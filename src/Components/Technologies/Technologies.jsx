ebb1c1826ed8f9555e140e4413e4cb3234dfa2fc
Fat2CS <coulibalyfatoumata@hotmail.com>
2023-03-23 16:17:45 +0100

    nextjs

--- src/Components/Technologies/Technologies.jsx
+++ src/Components/Technologies/Technologies.jsx
@@ -8,6 +8,7 @@ import Java from "../../img/javascript.png";
 import Css from "../../img/css.png";
 import reac from "../../img/react.png";
 import Mongodb from "../../img/mongodb.png";
+import Next from "../../img/nxt.png";
 import Php from "../../img/php.png";
 import ts from "../../img/ts.png";
 import { motion } from "framer-motion";
@@ -63,6 +64,9 @@ const Technologies = () => {
           </div>
           <div className="w-secCercle">
             <img src={ts} alt="" />
+          </div>
+          <div className="w-secCercle">
+            <img src={Next} alt="" />
           </div>
         </motion.div>
         <div className="w-backCercle orangeCircle"></div>
